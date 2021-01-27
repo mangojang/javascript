d2b253a87b03713301347bac158d5cc0380df413
jihyeonjang994 <jihyeon.jang@use9.co.kr>
2021-01-27 16:00:08 +0900

    실패할 경우, 성공할 경우 추가

--- 지뢰찾기/지뢰찾기.js
+++ 지뢰찾기/지뢰찾기.js
@@ -1,9 +1,23 @@
 const tbody = document.querySelector("#table tbody");
+let  dataset =[];
+let stop_flag = false;
+let open_square = 0;
+
 
 document.querySelector("#exec").addEventListener("click",function(){
+    //0. 내부 초기화
+    tbody.innerHTML="";
+    document.getElementById("result").textContent='';
+    dataset=[];
+    stop_flag=false;
+    open_square = 0;
+
+
+
     const hor = parseInt(document.querySelector("#hor").value);
     const ver = parseInt(document.querySelector("#ver").value);
     const mine = parseInt(document.querySelector("#mine").value);
+    
     //2. 지뢰 심을 자리 랜덤 뽑기
     const candidate = Array(hor*ver).fill().map(function(value,idx){
         return idx;
@@ -17,22 +31,24 @@ document.querySelector("#exec").addEventListener("click",function(){
     //1. 지뢰 판 만들기
     //이차배열
     //1-1. 세로 (tr) 만들기
-    //초기화
-    tbody.innerHTML="";
-    let  dataset =[];
+    
+    
     for(let i=0; i<ver; i++){
         let arr= [];
         dataset.push(arr);
         const tr = document.createElement('tr');
         //1-2. 가로 (td) 만들기
         for( let j=0; j<hor; j++){
-            arr.push(1);
+            arr.push(0);
             const td = document.createElement('td');
-            tr.appendChild(td);
             //4. 마우스 오른쪽 버튼 클릭시, 이벤트 생성 (깃발 꽂기)
             td.addEventListener('contextmenu',function(e){
                 // 컨텍스트 메뉴창 뜨는거 막기
                 e.preventDefault();
+                //플래그 함수 설정
+                if(stop_flag){
+                    return;
+                }
                 //클릭시, 줄 칸 몇번째 인지 알아내기
                 const parent_tr = e.currentTarget.parentNode;
                 const parent_tbody =  e.currentTarget.parentNode.parentNode;
@@ -62,17 +78,28 @@ document.querySelector("#exec").addEventListener("click",function(){
             });
             //5. 마우스 왼쪽 버튼 클릭시, 이벤트 생성 (주변 지뢰 개수 세기)
             td.addEventListener('click',function(e){
+                if(stop_flag){
+                    return;
+                }
+                
                 const parent_tr = e.currentTarget.parentNode;
                 const parent_tbody =  e.currentTarget.parentNode.parentNode;
                 const column = Array.prototype.indexOf.call(parent_tr.children,e.currentTarget);
                 const row= Array.prototype.indexOf.call(parent_tbody.children,parent_tr);
                 
+                if(dataset[row][column]===1){
+                    return;
+                }
                 e.currentTarget.classList.add('opend');
-
+                open_square += 1;
+                console.log('클릭 갯수', open_square);
                 if(dataset[row][column]==='X'){
                     e.currentTarget.textContent='펑';
+                    stop_flag= true;
+                    document.getElementById("result").textContent='실패!!!'
                 }else{
-                    let arround = [dataset[row][column-1],dataset[row][column+1]];
+                    dataset[row][column] = 1;
+                    let arround = [dataset[row][column-1],dataset[row][column+1],];
                     if(dataset[row-1]){
                         arround= arround.concat([ dataset[row-1][column-1],dataset[row-1][column],dataset[row-1][column+1] ])
                     }
@@ -81,7 +108,8 @@ document.querySelector("#exec").addEventListener("click",function(){
                     }
                     console.log ('around',arround);
                     const arround_mine_count = arround.filter(function(v){return v==='X'}).length;
-                    e.currentTarget.textContent = arround_mine_count; 
+                    e.currentTarget.textContent = arround_mine_count || ''; 
+                    console.log('count', typeof(arround_mine_count));
                 
                     // 6. 클릭시, 주변 x가 0 이면 주변 8칸 동시 오픈 
                     //6-1. 주변칸을 배열로 모은다
@@ -96,7 +124,6 @@ document.querySelector("#exec").addEventListener("click",function(){
                         }
                         arround_square = arround_square.concat([
                             tbody.children[row].children[column-1],
-                            tbody.children[row].children[column],
                             tbody.children[row].children[column+1],
                         ]);
                         if(tbody.children[row+1]){
@@ -106,14 +133,38 @@ document.querySelector("#exec").addEventListener("click",function(){
                                 tbody.children[row+1].children[column+1],
                             ]);
                         }
+                        console.log('as',arround_square);
+                        // arround_square[0].click();
+                        //배열에서 undifind, null, 0을 제거하는 코드 
+                        arround_square.filter(function(v){
+                            return !!v;
+                        })
+                        console.log('length',arround_square.length);
+                        // for (let index = 0; index < arround_square.length; index++) {
+                        //     arround_square[index].click();
+                            
+                        // }
+                        
+                        arround_square.forEach(function(next_square,idx){
+                            console.log(next_square);
+                            const pparent_tr = next_square.parentNode;
+                            const pparent_tbody =  next_square.parentNode.parentNode;
+                            const next_column_column = Array.prototype.indexOf.call(pparent_tr.children,next_square);
+                            const next_column_row= Array.prototype.indexOf.call(pparent_tbody.children,pparent_tr);
+                            if(dataset[next_column_row][next_column_column]!==1){
+                                next_square.click();
+                            }
+                        });
                     }
-                    //배열에서 undifind, null, 0을 제거하는 코드 
-                    arround_square.filter(function(v){return !!v}).forEach(function(next_square){
-                        next_square.click();
-                    });
+                }
+                
+
+                if (open_square === hor*ver-mine){
+                    stop_flag=true;
+                    document.getElementById('result').textContent='성공!!!'
                 }
             });
-            
+            tr.appendChild(td);
         }
         tbody.appendChild(tr);        
     }
